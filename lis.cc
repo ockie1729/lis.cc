87d57d27e90df61024405603bd47041128699d9c
mofumofuchan <you_oki1@yahoo.co.jp>
2021-05-07 21:22:18 +0900

    インタプリタの雛形を作成

---
+++ lis.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "lis.h"
+
+void repl() {
+    std::cout << "hello world!!" << std::endl;
+
+    while (true) {
+        std::cout << "lis.cpp> ";
+       
+        std::string s;
+        std::cin >> s;
+
+        std::cout << s << std::endl;
+    }
+}
---
+++ main.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "lis.h"
+
+int main() {
+    repl();
+}
