428cd901dc0ebf039d3b31b6f2beefcf50a88731
mofumofuchan <you_oki1@yahoo.co.jp>
2021-05-10 23:53:24 +0900

    変数名を変更

--- lis.cc
+++ lis.cc
@@ -27,33 +27,33 @@ bool is_whitespace(const char c) {
 }
 
 
-std::vector<std::string> split_str(const std::string input_str) {
+std::vector<std::string> split_str(const std::string original_str) {
   std::vector<std::string> split_tokens;
   std::string::size_type idx_token_start = -1;  // FIXME
   bool is_idx_in_token = false;
 
-  for (std::string::size_type idx = 0; idx < input_str.size(); idx++) {
+  for (std::string::size_type idx = 0; idx < original_str.size(); idx++) {
     // 行末以外でトークンの終わりを見つけた時
-    if (is_idx_in_token && is_whitespace(input_str[idx])) {
+    if (is_idx_in_token && is_whitespace(original_str[idx])) {
       is_idx_in_token = false;
 
       std::string token =
-        input_str.substr(idx_token_start, idx-idx_token_start);
+        original_str.substr(idx_token_start, idx-idx_token_start);
       split_tokens.push_back(token);
     }
 
     // 行末でトークンの終わりを見つけた時
-    if (is_idx_in_token && !(is_whitespace(input_str[idx])) &&
-          (idx == input_str.size()-1)) {
+    if (is_idx_in_token && !(is_whitespace(original_str[idx])) &&
+          (idx == original_str.size()-1)) {
       is_idx_in_token = false;
 
       const std::string token =
-        input_str.substr(idx_token_start, (idx-idx_token_start)+1);
+        original_str.substr(idx_token_start, (idx-idx_token_start)+1);
       split_tokens.push_back(token);
     }
 
     // トークンの始まりを見つけた時
-    if (!(is_idx_in_token) && !(is_whitespace(input_str[idx]))) {
+    if (!(is_idx_in_token) && !(is_whitespace(original_str[idx]))) {
       is_idx_in_token = true;
       idx_token_start = idx;
     }
